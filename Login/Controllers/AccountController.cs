1b424cc9d0f52e64a51cc9a26070ada43948f8ea
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2025-09-01 20:59:30 -0600

    Agregar archivos de proyecto.

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Login (LoginViewModel model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+
+                if(model.Username == "admin")
+                {
+                    //username: admin
+                    //password: 1234
+
+                    ViewBag.Mensaje = "Login exitoso";
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Usuario o contraseña incorrectos");
+                }
+
+
+                return View(model);
+            }
+
+            return View(model);
+  
+          
+        }
+
+    }
+  
+}
---
+++ Login/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Login.Models
+{
+    public class LoginViewModel
+    {
+        
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+       
+
+
+    }
+}
+
