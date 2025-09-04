728be59cfd62da49e4fb9b8481bc8181679e279b
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2025-09-03 20:35:03 -0600

    nuevos

---
+++ Login/Controllers/PersonaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class PersonaController : Controller
+    {
+        
+        public ActionResult Persona()
+        {
+            return View();
+        }
+
+        public ActionResult ListaPersona()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Login/Models/PersonaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Login.Models
+{
+    public class PersonaViewModel
+    {
+        public int IdPersona { get; set; }
+        public string? Nombre { get; set; } 
+        public int? Edad { get; set; }
+         
+    }
+}
